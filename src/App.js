be8b379c0bf607f74838570f59565c639d81d712
Nisha Bhooshan <bhooshnnisha@gmail.com>
2021-04-10 02:09:52 +0530

    Added new Component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import {Route,Switch} from "react-router-dom"
 import "./App.css";
 import HomePageComponent from "./pages/homepage/homePageComponent"
 import ShopPage from "./pages/homepage/shop/shop"
-
+import Header from "./components/header/header"
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
     <Route exact path="/" component={HomePageComponent}/>
     <Route path="/shop" component={ShopPage}/>
